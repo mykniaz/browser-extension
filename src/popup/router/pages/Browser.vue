4a9b3e14aca2568efcc69ac3603e0f6e2ca56863
mykniaz <mykniaz@gmail.com>
2019-05-14 22:52:56 +0300

    fe add options page

--- src/popup/router/pages/Browser.vue
+++ src/popup/router/pages/Browser.vue
@@ -1,12 +1,6 @@
 <template>
   <div :style="{ backgroundColor: backgroundColor }">
     <h1>
-      Текущая вкладка
-    </h1>
-    <div>
-      {{ tabInfo }}
-    </div>
-    <h1>
       Открытые вкладки
     </h1>
     <div>
@@ -23,7 +17,6 @@ export default {
   name: 'Browser',
 
   data: () => ({
-    tabInfo: {},
     tabsInfo: [],
   }),
 
@@ -32,9 +25,6 @@ export default {
   },
 
   created() {
-    this.getTabsByFilter({ active: true, windowId: browser.windows.WINDOW_ID_CURRENT }).then(tabs => {
-      this.tabInfo = tabs[0];
-    });
     this.tabsInfo = this.getTabsByFilter({ currentWindow: true }).then(tabs => {
       this.tabsInfo = tabs;
     });
